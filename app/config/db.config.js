11e0ee3a5f92de9028fd222fb786a06741f66a4c
beyzaerkan <beyzaerkan@icloud.com>
2021-06-09 17:32:42 +0300

    CRUD islemleri yapıldı

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+module.exports = {
+  HOST: process.env.HOST,
+  USER: process.env.DATABASE_USERNAME,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  DB: process.env.DATABASE,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
